ee793b183fddd08e9f765de71a80ab81e553dffd
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-05-27 23:01:52 +0600

    added nested components in main components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
 
-function Greeting(){
-  return <h4>Hey its me sabbir.The boy </h4>
+function Greeting() {
+  return (
+    <div>
+      <h4>Hey its me sabbir.The boy</h4>
+      <Test1 />
+      <Test2 />
+    </div>
+  );
 }
-ReactDom.render(<Greeting/>, document.getElementById('root'));
+const Test1 = () => {
+  return <h1>Test1</h1>;
+};
+const Test2 = () => {
+  return <h1>Test2</h1>;
+};
+ReactDom.render(<Greeting />, document.getElementById("root"));
