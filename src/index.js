2df22366693d526f55810b5a5bea2f833c50e666
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-05-28 12:56:36 +0600

    added simple list

--- src/index.js
+++ src/index.js
@@ -1,40 +1,27 @@
 import React from "react";
 import ReactDom from "react-dom";
-const firstbook = {
-  author: "sabbirdk",
-  title: "I know who am I",
-  image:
-    "https://m.media-amazon.com/images/I/51+CYfFksWS._BG0,0,0,0_FMpng_AC_SY160_SX160_.jpg",
-};
-const secoundbook = {
-  author: "sabbir",
-  title: "I know  am I",
-  image:
-    "https://m.media-amazon.com/images/I/51+CYfFksWS._BG0,0,0,0_FMpng_AC_SY160_SX160_.jpg",
-};
-
+const books = [
+  {
+    author: "sabbirdk",
+    title: "I know who am I",
+    image:
+      "https://m.media-amazon.com/images/I/51+CYfFksWS._BG0,0,0,0_FMpng_AC_SY160_SX160_.jpg",
+  },
+  {
+    author: "sabbir",
+    title: "I know  am I",
+    image:
+      "https://m.media-amazon.com/images/I/51+CYfFksWS._BG0,0,0,0_FMpng_AC_SY160_SX160_.jpg",
+  },
+];
+const name = ["sabbir", "sawon", "alifa"];
+const newNames = name.map((sabbir) => {
+  return <h1>{sabbir}</h1>;
+  // console.log(newNames);
+});
+console.log(newNames);
 function BookList() {
-  return (
-    <div>
-      <h4>Hey its me sabbir.The boy</h4>
-      <Book
-        author={firstbook.author}
-        title={firstbook.title}
-        image={firstbook.image}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit illum
-          nesciunt obcaecati velit natus quasi nemo excepturi dolor ipsa quo
-          soluta odit quod totam quas ducimus animi, magni iste voluptates.
-        </p>
-      </Book>
-      <Book
-        author={secoundbook.author}
-        title={secoundbook.title}
-        image={secoundbook.image}
-      />
-    </div>
-  );
+  return <section>{newNames}</section>;
 }
 const Book = ({ image, title, author, children }) => {
   // console.log(props);
