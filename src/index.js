89ce812a34b6f9eab4140d5eb548a2d1a91a5f73
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-05-28 13:17:17 +0600

    added proper list

--- src/index.js
+++ src/index.js
@@ -14,23 +14,29 @@ const books = [
       "https://m.media-amazon.com/images/I/51+CYfFksWS._BG0,0,0,0_FMpng_AC_SY160_SX160_.jpg",
   },
 ];
-const name = ["sabbir", "sawon", "alifa"];
-const newNames = name.map((sabbir) => {
-  return <h1>{sabbir}</h1>;
-  // console.log(newNames);
-});
-console.log(newNames);
+// const name = ["sabbir", "sawon", "alifa"];
+// const newNames = name.map((sabbir) => {
+//   return <h1>{sabbir}</h1>;
+// console.log(newNames);
+// });
+// console.log(newNames);
 function BookList() {
-  return <section>{newNames}</section>;
+  return (
+    <section>
+      {books.map((boi) => {
+        return <Book boi={boi} />;
+      })}
+    </section>
+  );
 }
-const Book = ({ image, title, author, children }) => {
+const Book = (props) => {
   // console.log(props);
+  const { image, title, author } = props.boi;
   return (
     <section>
       <img src={image} alt="" />
       <h1>{title}</h1>
       <p>{author}</p>
-      <p>{children}</p>
     </section>
   );
 };
