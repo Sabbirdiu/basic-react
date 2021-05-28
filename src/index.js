84ecf3d291e9028d8cdcc7a9bab544559c317f35
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-05-28 19:17:03 +0600

    added key prop and spread operators

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDom from "react-dom";
 const books = [
   {
+    id: 1,
     author: "sabbirdk",
     title: "I know who am I",
     image:
       "https://m.media-amazon.com/images/I/51+CYfFksWS._BG0,0,0,0_FMpng_AC_SY160_SX160_.jpg",
   },
   {
+    id: 2,
     author: "sabbir",
     title: "I know  am I",
     image:
@@ -24,14 +26,14 @@ function BookList() {
   return (
     <section>
       {books.map((boi) => {
-        return <Book boi={boi} />;
+        return <Book key={boi.id} {...boi} />;
       })}
     </section>
   );
 }
-const Book = (props) => {
+const Book = ({ image, title, author }) => {
   // console.log(props);
-  const { image, title, author } = props.boi;
+  // const { image, title, author } = props;
   return (
     <section>
       <img src={image} alt="" />
