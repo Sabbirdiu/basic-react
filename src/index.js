3cfacc972c687f519daf650ea3f10acc00313e7b
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-05-28 19:43:01 +0600

    event basic

--- src/index.js
+++ src/index.js
@@ -32,14 +32,24 @@ function BookList() {
   );
 }
 const Book = ({ image, title, author }) => {
+  const TestEvent = () => {
+    alert("event is working");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   // console.log(props);
   // const { image, title, author } = props;
   return (
-    <section>
+    <article onMouseOver={() => console.log(title)}>
       <img src={image} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <p>{author}</p>
-    </section>
+      <button onClick={TestEvent}>Test Event</button>
+      <p>
+        <button onClick={complexExample(author)}>complex event</button>
+      </p>
+    </article>
   );
 };
 
